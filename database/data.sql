ab323e30d07bb8668c1d083ea1b4af0ee9d4ae62
Johnny Baltazar <baltazarjohnny@yahoo.com>
2022-10-26 21:52:37 -0700

    Update data.sql to insert into users, userId, userName, firstName, lastName, email, hashedPassword, location, and details. #1

--- database/data.sql
+++ database/data.sql
@@ -1,2 +1,3 @@
-insert into "users" ("userId", "username", "firstName", "lastName", "email", "hashedPassword", "location", "details")
-values ($1, 'anonymous', $3, $4, $5, '$argon2i$v=19$m=4096,t=3,p=1$h7icQD/xZr8akZsX+hNA0A$h68atJWyjvunAwNOpSpMfg9sPvoMQ6dKwoh0dJhurWA', $7, $8);
+insert into "users" ("username", "firstName", "lastName", "email", "hashedPassword", "location", "details")
+values
+  ('anonymous', 'Test', 'User', 'test@mail.com', '$argon2i$v=19$m=4096,t=3,p=1$h7icQD/xZr8akZsX+hNA0A$h68atJWyjvunAwNOpSpMfg9sPvoMQ6dKwoh0dJhurWA', 'Irvine, CA', 'This is a test user');
