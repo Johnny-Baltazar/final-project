b3569be623dda934ac8372e5053818b7d786af64
Johnny Baltazar <baltazarjohnny@yahoo.com>
2022-10-26 21:56:28 -0700

    Create a component for a NavBar. #1

--- client/components/navbar.jsx
+++ client/components/navbar.jsx
@@ -1,28 +1,27 @@
 import React from 'react';
 import AppContext from '../lib/app-context';
 import AppDrawer from './app-drawer';
-
 export default class Navbar extends React.Component {
-
   render() {
     const { user, route } = this.context;
-    const loginOrRegister = route.path === 'sign-up'
+    const loginOrRegister = route.path === 'sign-up' && user === null
       ? 'Login'
       : 'Register';
+    const navAnc = route.path === 'sign-up'
+      ? '#sign-in'
+      : '#sign-up';
     return (
       <nav className="navbar">
-        <AppDrawer />
+        <AppDrawer/>
         <h3>
           My Travel Log
         </h3>
         <div>
-          { user === null &&
-            <a href="#sign-in" >
-              <i>
-                {loginOrRegister}
-              </i>
-            </a>
-          }
+          <a href={navAnc} >
+            <i>
+              {loginOrRegister}
+            </i>
+          </a>
         </div>
       </nav>
     );
